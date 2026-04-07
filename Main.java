df538db8377545b19fc66660a55b94c4fc7469a8
ashish-sonagara <aashishsonagara0@gmail.com>
2026-04-07 23:42:17 +0530

    develop - connected the local file to save data, learned file handling

--- Main.java
+++ Main.java
@@ -1,88 +1,153 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
+
 import Enums.AccountType;
+import Enums.TransactionType;
 import Model.*;
 import Services.*;
 
 public class Main {
     public static void main(String args[]) {
-        // System.out.println("Project starts..");
         AccountService accountService = new AccountService();
         TransactionService transactionService = new TransactionService();
         BankAccount b1 = new BankAccount(AccountType.CURRENT, 4004, 20000.0);
         BankAccount b2 = new BankAccount(AccountType.SAVING, 5005, 19000);
 
-        try{
-            String depositedResult = accountService.deposit(b1, 1800);
-            System.out.println(depositedResult);
-            System.out.println();
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
+        accountService.readFile(TransactionType.DEPOSIT);
+        // try{
+        //     String depositedResult = accountService.deposit(b1, 1800);
+        //     System.out.println(depositedResult);
+        //     System.out.println();
+        // }
+        // catch(Exception e){
+        //     System.out.println(e);
+        // }
 
-        try {
-            String withdrawnResult = accountService.withdraw(b1, 2000);
-            System.out.println(withdrawnResult);
-            System.out.println();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        // try {
+        //     String withdrawnResult = accountService.withdraw(b1, 2000);
+        //     System.out.println( );
+        //     System.out.println();
+        // } catch (Exception e) {
+        //     System.out.println(e);
+        // }
 
-        String checkBalanceResult = accountService.checkBankBalance(b1);
-        System.out.println(checkBalanceResult);
-        System.out.println();
+        // try{
+        //     String depositedResult = accountService.deposit(b2, 3800);
+        //     System.out.println(depositedResult);
+        //     System.out.println();
+        // }
+        // catch(Exception e){
+        //     System.out.println(e);
+        // }
+
+        // try {
+        //     String withdrawnResult = accountService.withdraw(b2, 2100);
+        //     System.out.println( );
+        //     System.out.println();
+        // } catch (Exception e) {
+        //     System.out.println(e);
+        // }
+        // String checkBalanceResult = accountService.checkBankBalance(b1);
+        // System.out.println(checkBalanceResult);
+        // System.out.println();
 
         // System.out.println("For Amount Transfer ");
-        try {
-            String transferResult = accountService.transfer(b1, b2, 10000);
-            System.out.println(transferResult);
-            System.out.println();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        // try {
+        //     String transferResult = accountService.transfer(b1, b2, 10000);
+        //     System.out.println(transferResult);
+        //     System.out.println();
+        // } catch (Exception e) {
+        //     System.out.println(e);
+        // }
 
-        transactionService.showAllTransactions(b1);
-        transactionService.showAllTransactions(b2);
+        // transactionService.showAllTransactions(b1);
+        // transactionService.showAllTransactions(b2);
 
-        try{
-            List<Transaction> transactions = transactionService.lastNTransactions(b1, 2);
-            System.out.println("------ Last 2 Transactions------------");
-            for(Transaction t : transactions){
-                System.out.println("Found Transaction of type " + t.getTransactionType() + " of amount " + t.getTransactionAmount());
-            }
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
+        // try{
+        //     List<Transaction> transactions = transactionService.lastNTransactions(b1, 2);
+        //     System.out.println("------ Last 2 Transactions------------");
+        //     for(Transaction t : transactions){
+        //         System.out.println("Found Transaction of type " + t.getTransactionType() + " of amount " + t.getTransactionAmount());
+        //     }
+        // }
+        // catch(Exception e){
+        //     System.out.println(e);
+        // }
 
-        System.out.println();
-        List<Transaction> ts = transactionService.transactionBetweenDates(b1, LocalDate.of(2026, 04, 02), LocalDate.of(2026, 04, 03));
-        for(Transaction transaction : ts){
-            System.out.println("Transaction of type " + transaction.getTransactionType() + " of amount " + transaction.getTransactionAmount());
-        }
+        // System.out.println();
+        // List<Transaction> ts = transactionService.transactionBetweenDates(b1, LocalDate.of(2026, 04, 02), LocalDate.of(2026, 04, 03));
+        // for(Transaction transaction : ts){
+        //     System.out.println("Transaction of type " + transaction.getTransactionType() + " of amount " + transaction.getTransactionAmount());
+        // }
 
-        System.out.println();
-        List<Transaction> highTransaction = transactionService.highValueTransaction(b1, 2000);
-        System.out.println("----------- High Value Transactons -------------");
-        for(Transaction transaction : highTransaction){
-            System.out.println("Transaction of type " + transaction.getTransactionType() + " of amount " + transaction.getTransactionAmount());
-        }
+        // System.out.println();
+        // List<Transaction> highTransaction = transactionService.highValueTransaction(b1, 2000);
+        // System.out.println("----------- High Value Transactons -------------");
+        // for(Transaction transaction : highTransaction){
+        //     System.out.println("Transaction of type " + transaction.getTransactionType() + " of amount " + transaction.getTransactionAmount());
+        // }
 
-        System.out.println();
-        // System.out.println(" -----------show all transaction of bank Account ----------------");
-        transactionService.showAllTransactions(b1);
+        // System.out.println();
+        // // System.out.println(" -----------show all transaction of bank Account ----------------");
+        // transactionService.showAllTransactions(b1);
 
-        System.out.println();
-        System.out.println("-----------Average Amount of the Transaction-----------");
-        double averageValue = transactionService.getAverageTransactionAmount(b1);
-        System.out.println(averageValue);
+        // System.out.println();
+        // System.out.println("-----------Average Amount of the Transaction-----------");
+        // double averageValue = transactionService.getAverageTransactionAmount(b1);
+        // System.out.println(averageValue);
 
-        System.out.println();
-        System.out.println("----------------- Total Expense--------------");
-        double totalAmount = transactionService.getTotalExpenses(b1);
-        System.out.println(totalAmount);
+        // System.out.println();
+        // System.out.println("----------------- Total Expense--------------");
+        // double totalAmount = transactionService.getTotalExpenses(b1);
+        // System.out.println(totalAmount);
+
+        // -----------------------------------------------------------------------------
+
+        
+        // File myFile = new File("bankFile.txt");
+        // try {
+        //     myFile.createNewFile();
+        // } catch (IOException e) {
+        //     System.out.println("Error While handling the file." + e);
+        // }
+
+        // try{
+        //     FileWriter wr = new FileWriter("bankFile.txt");
+        //     wr.write("BANK STATEMENTS");
+        //     wr.close();
+        // }
+        // catch(IOException e){
+        //     System.out.println("Error while writing inside the file" + e);
+        // }
+
+        // try{
+        //     File readFile = new File("bankFile.txt");
+        //     Scanner reader = new Scanner(readFile);
+        //     while(reader.hasNextLine()){
+        //         String data = reader.nextLine();
+        //         System.out.println(data);
+        //     }
+        // }
+        // catch(IOException e){
+        //     System.out.println("Error while reading File" + e);
+        // }
+
+        // try( BufferedReader br = new BufferedReader(new FileReader("bankFile.txt"))){
+        //     String line;
+        //     while((line = br.readLine()) != null){
+        //         System.out.println(line);
+        //     }
+        // }
+        // catch(IOException e){
+        //     System.out.println("Error while Reading File" + e);
+        // }
     }
 }
--- Services/AccountService.java
+++ Services/AccountService.java
@@ -1,5 +1,7 @@
 package Services;
 
+import java.io.*;
+// import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -11,6 +13,29 @@ import Model.*;
 
 public class AccountService {
     Random r= new Random();
+    private static File despositTransaction = new File("DepositTransactionFile.txt");
+    private static File withdrawnTransaction = new File("WithDrawnTransactionFile.txt");
+
+    static {    
+        try {
+            // if(despositTransaction.createNewFile()){
+            //     System.out.println("New File Created");
+            // }
+            // else{
+            //     System.out.println("File With the name Already Exit");
+            // }
+            despositTransaction.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Error while creating a despositTransaction file " + e);
+        }
+
+        try{
+            withdrawnTransaction.createNewFile();
+        }
+        catch(IOException e){
+            System.out.println("Error while creating withdrawnTransaction File" + e);
+        }
+    }
 
     public String deposit(BankAccount bankAccount, double amount) throws InvalidAmountException {
         if (amount > 10000) {
@@ -23,22 +48,24 @@ public class AccountService {
         List<Transaction> transactions = new ArrayList<>();
         transactions.add(t);
         bankAccount.setTransactionHistory(transactions);
+        fileWriting(t);
         return amount + " deposited in the " + bankAccount.getAccountNumber();
     }
 
     public String withdraw(BankAccount bankAccount, double amount) throws InsufficientBalanceException {
         if (bankAccount.getCurrentBalance() < amount) {
             throw new InsufficientBalanceException("Withdrawn Amount is greater than the Current Bank balance");
-        } else {
-            double currBalance = bankAccount.getCurrentBalance();
-            currBalance -= amount;
-            bankAccount.setCurrentBalance(currBalance);
-            Transaction t = new Transaction(TransactionType.WITHDRAW, "-" + amount, r.nextInt(1, 1001));
-            List<Transaction> transactions = new ArrayList<>();
-            transactions.add(t);
-            bankAccount.setTransactionHistory(transactions);
-            return amount + " withdrawn from the " + bankAccount.getAccountNumber();
         }
+        double currBalance = bankAccount.getCurrentBalance();
+        currBalance -= amount;
+        bankAccount.setCurrentBalance(currBalance);
+        Transaction t = new Transaction(TransactionType.WITHDRAW, "-" + amount, r.nextInt(1, 1001));
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(t);
+        bankAccount.setTransactionHistory(transactions);
+        fileWriting(t);
+
+        return amount + " withdrawn from the " + bankAccount.getAccountNumber();
     }
 
     public String checkBankBalance(BankAccount bankAccount) {
@@ -74,6 +101,36 @@ public class AccountService {
         bankAccount2.setTransactionHistory(transactions2);
 
         return "Transferred " + amount + " from Bank Account " + bankAccount1.getAccountNumber() + " to " + bankAccount2.getAccountNumber();
+    }
+
+    public void fileWriting(Transaction transaction) {
+        String logEntry = "ID: " + transaction.getTransactionId() +
+                " | Type: " + transaction.getTransactionType() +
+                " | Amount: " + transaction.getTransactionAmount() +
+                " | Date: " + transaction.getDate() + "\n";
+
+        String fileName = transaction.getTransactionType().toString().equals("DEPOSIT") ? "DepositTransactionFile.txt" : "WithDrawnTransactionFile.txt";
+
+        try(FileWriter wr = new FileWriter(fileName , true)){
+            wr.write(logEntry);
+        }
+        catch(IOException e){
+            System.out.println("Error While Writing Into a File" + e);
+        }
+    }
+
+    public void readFile(TransactionType transactionType){
+        String fileName = transactionType.toString().equals("DEPOSIT") ? "DepositTransactionFile.txt" : "WithDrawnTransactionFile.txt";
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            String line;
+            while (( line = br.readLine()) != null){
+                System.out.println(line);
+            } 
+        }
+        catch(IOException e){
+            System.out.println("Error while Reading File" + e);
+        }
     }
 
 }
